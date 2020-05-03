16f065c3e40b1e1cb716da7c089e9bcdcf570cd5
shinahu <61527348+shinahu@users.noreply.github.com>
2020-05-03 21:19:33 +1000

    submit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_12
+{
+    class Program
+    {
+        static int Fib(int n1)
+ {
+      if (n1 <=2)
+      return 1;
+      else
+      return Fib( n1 - 1 ) + Fib( n1 - 2 );
+ }
+public static void Main()
+ {
+      int num;
+  
+	     
+      Console.Write("Enter a number: ");
+      num = Convert.ToInt32( Console.ReadLine() );
+   
+      Console.WriteLine("\nThe Fibonacci of {0} th term  is {1} \n", num, Fib(num));
+ }
+    }
+}
